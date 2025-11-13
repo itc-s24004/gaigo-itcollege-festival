01284d9f1a12d53c3695fa65e7b506b7deb7c084
s24004 <s24004@std.it-college.ac.jp>
2025-11-13 10:24:44 +0900

    ビルドのために修正

---
+++ app/admin/editor/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+    return <div>Food Event Page</div>;
+}
--- app/api/auth/update/route.tsx
+++ app/api/auth/update/route.tsx
@@ -1,16 +1,31 @@
 import { getUserInfo } from "@/libs/user";
 import { NextResponse } from "next/server";
 
-export async function POST(req: Request, res: Response) {
+export async function POST(req: Request) {
     const userInfo = await getUserInfo();
     const user = userInfo.user;
 
-    if (!user) return new Response("Not logged in.", { status: 401 });
+    if (!user) return NextResponse.json(
+        {
+            message: "User not logged in."
+        },
+        { status: 401 }
+    );
 
     const form = await req.formData();
     const newNickName = form.get("nickName") as string | null;
 
-    if (!newNickName || newNickName.length < 3 || newNickName.length > 32) return
+    if (!newNickName || newNickName.length < 3 || newNickName.length > 32) return NextResponse.json(
+        {
+            message: "Invalid nickname. It must be between 3 and 32 characters."
+        },
+        { status: 400 }
+    );
 
-    return new Response("Nickname updated successfully.", { status: 200 });
+    return NextResponse.json(
+        {
+            message: "Nickname updated successfully."
+        },
+        { status: 200 }
+    );
 }
