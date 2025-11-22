2de140bcf91e74a28a47b1b2d069f18ba62b9972
s24004 <s24004@std.it-college.ac.jp>
2025-11-22 10:46:20 +0900

    使用していないコンポーネントを削除

--- page_components/app/post/index.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export function PostForm() {
-    return (
-        <div>Post Form</div>
-    );
-}
-
-//!!! 未完成
--- page_components/tool/select_view/index.tsx
+++ page_components/tool/select_view/index.tsx
@@ -1,72 +1,7 @@
 "use client";
 
-import { useState } from "react";
 
 import styles from "./index.module.css";
-
-type Options = {
-    options: {
-        element: React.ReactNode;
-        value: string;
-    }[];
-    multiple?: boolean;
-    submitLabel?: string;
-    submitCallback?: (value: string[]) => void;
-    customAttributes?: React.HTMLAttributes<HTMLDivElement>;
-    customOptionAttributes?: React.HTMLAttributes<HTMLDivElement>;
-    customSelectedOptionAttributes?: React.HTMLAttributes<HTMLDivElement>;
-    customSubmitButtonAttributes?: React.HTMLAttributes<HTMLButtonElement>;
-}
-export function SelectForm({
-    options, multiple, submitLabel,
-    submitCallback, customAttributes,
-    customSelectedOptionAttributes,
-    customOptionAttributes,
-    customSubmitButtonAttributes
-}: Options) {
-
-    const [selectedOptions, setSelectedOptions] = useState<number[]>([]);
-    return (
-        <div {...customAttributes}>
-            {
-                options.map((option, i) => {
-                    const isSelected = selectedOptions.includes(i);
-                    return (
-                        <div key={i}
-                            // デフォルトのスタイルと選択時のスタイルを切り替え
-                            className={isSelected ? `${styles.option} ${styles.selected}` : styles.option}
-                            // カスタムオプション属性を切り替え
-                            {...(isSelected ? customSelectedOptionAttributes : customOptionAttributes)}
-                            // 選択・解除の処理
-                            onClick={() => {
-                                if (multiple) {
-                                    if (isSelected) {
-                                        setSelectedOptions(selectedOptions.filter((v) => v !== i));
-                                    } else {
-                                        setSelectedOptions([...selectedOptions, i]);
-                                    }
-                                } else {
-                                    setSelectedOptions([i]);
-                                }
-                            }}
-                        >
-
-                            {option.element}
-
-                        </div>
-                    )
-                })
-            }
-            <button
-                {...customSubmitButtonAttributes}
-                onClick={() => submitCallback && submitCallback(selectedOptions.map(i => options[i].value))}
-            >
-                {submitLabel}
-            </button>
-        </div>
-    )
-}
-
 
 
 
